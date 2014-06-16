d9ba48d850b5c53906494b57ea86af4577a724cd
simsum <sven@Svens-MacBook-Pro.local>
2014-06-16 14:47:39 +0200

    New Function

--- DTutils.cpp
+++ DTutils.cpp
@@ -26,3 +26,18 @@ int LeapYear (int Year)                     // Schaltjahr aktiv (1 = Ja)
     int sj = (1/(1 + Year % 4)) - (1/(1 + Year % 100)) + (1/(1 + Year % 400));
     return sj;
 }
+
+// noch zu testende Funktionen !!!
+
+//Die Funktion SUN_MIDDAY berechnet abhängig vom Tagesdatum zu welcher Tageszeit die Sonne exakt im Süden steht.
+//Die Berechnung erfolgt in UTC (Weltzeit).
+//Lon = Längengrad des Bezugsortes.
+
+float sunMiddayTimeUTC (float Lon, int Year, int Month, int Day)
+{
+    float T = DayOfYear(Year, Month, Day);
+    float OFFSET = -0.1752 * sin(0.033430 * T + 0.5474) - 0.1340 * sin(0.018234 * T - 0.1939);
+    float SunMidday = (12.0 - OFFSET - Lon * 0.0666666666666);
+    return SunMidday;
+    
+}
